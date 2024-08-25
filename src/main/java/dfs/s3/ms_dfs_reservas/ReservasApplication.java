590929b615b285efe824458b3ea6e4bdbea5d843
puffoide <fe.salgadot@duocuc.cl>
2024-08-25 04:19:35 -0400

    Implementación de endpoints
    Se implementa:
    
    Endpoint de citas reservadas
    Endpoint de citas según id
    Endpoint de horarios disponibles

--- src/main/java/dfs/s3/ms_dfs_reservas/ReservasApplication.java
+++ src/main/java/dfs/s3/ms_dfs_reservas/ReservasApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsDfsReservasApplication {
+public class ReservasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsDfsReservasApplication.class, args);
+		SpringApplication.run(ReservasApplication.class, args);
 	}
 
 }
