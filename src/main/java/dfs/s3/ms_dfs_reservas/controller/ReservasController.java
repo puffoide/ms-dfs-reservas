7fd19e89ea5ae0816a24e83ce31585a6d3e3d006
puffoide <fe.salgadot@duocuc.cl>
2024-08-26 16:41:44 -0400

    Se añade ResponseEntity en los endpoints

--- src/main/java/dfs/s3/ms_dfs_reservas/controller/ReservasController.java
+++ src/main/java/dfs/s3/ms_dfs_reservas/controller/ReservasController.java
@@ -2,6 +2,7 @@ package dfs.s3.ms_dfs_reservas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -18,17 +19,23 @@ public class ReservasController {
     private ReservasService reservasService;
 
     @GetMapping("/citas")
-    public List<Cita> getCitas() {
-        return reservasService.getCitas();
+    public ResponseEntity<List<Cita>> getCitas() {
+        List<Cita> cita = reservasService.getCitas();
+        return ResponseEntity.ok(cita);
     }
 
     @GetMapping("/citas/{id}")
-    public Cita getCitaById(@PathVariable int id) {
-        return reservasService.getCitaById(id);
+    public ResponseEntity<Cita> getCitaById(@PathVariable int id) {
+        Cita cita = reservasService.getCitaById(id);
+        if (cita == null) {
+            return ResponseEntity.notFound().build();
+        } else
+            return ResponseEntity.ok(cita);
     }
 
     @GetMapping("/horarios")
-    public List<Horario> getHorariosDisponibles() {
-        return reservasService.getHorariosDisponibles();
+    public ResponseEntity<List<Horario>> getHorariosDisponibles() {
+        List<Horario> horarios = reservasService.getHorariosDisponibles();
+        return ResponseEntity.ok(horarios);
     }
 }
